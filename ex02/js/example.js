0738da21d33e7c156cb1e189e9616f989d84cea8
Gohyeee <130416946+Gohyeee@users.noreply.github.com>
2023-05-09 18:16:37 +0900

    [Feat] : 02

---
+++ ex02/js/example.js
@@ -0,0 +1,22 @@
+document.write("환영합니다.");
+
+//코드입력시 주의할점
+
+//1.자바스크립트는 객체 생성 시 대소문자 구분을 해주어야한다.
+new Date(); 
+//New Date(); 
+
+//2.코드 작성 후에는 ;를 무조건 붙여주어야 한다.
+//3.가독성을 위해 가급적 한 줄에 한 문장만 작성하는 것이 좋다
+document.write("hi");document.write("bye") ;
+//document.write("hi")document.write("bye") ;
+
+//4.문자형 데이터를 작성할 때 큰따옴표와 작은따옴표 겹침오류 주의
+document.write("say \"hello\"");
+document.write('say "hello"');
+//document.write("say "hello"");
+
+//5.코드 작성할때 {} () 짝이 맞아야한다
+document.write("welcome");
+//document.write("welcome";
+
